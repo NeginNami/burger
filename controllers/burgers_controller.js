4eb8cd0eca5960df04571edd5ffc736988820c3a
Negin <negin.namiranian@gmail.com>
2017-12-05 14:03:08 -0800

    Modified db,orm,model,handlebar

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,3 +5,15 @@ var router = express.Router();
 
 
 var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var obj = {
+      burgers: data
+    };
+    console.log(obj);
+    res.render("index", obj);
+  });
+});
+
+module.exports = router;
