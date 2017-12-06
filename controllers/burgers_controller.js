0c7bf83374baf46eced3e3f8bf7aa695fa124042
Negin <negin.namiranian@gmail.com>
2017-12-06 15:17:27 -0800

    Code cleaned up

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,6 @@ var express = require("express");
 
 var router = express.Router();
 
-
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
@@ -35,9 +34,5 @@ router.put("/api/burgers/:id",function (req,res) {
 	    }
 	});
 });
-
-
-
-
 
 module.exports = router;
