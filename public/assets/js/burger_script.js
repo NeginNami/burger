40d938a891d738e2c318ccba63e904fc757c7628
Negin <negin.namiranian@gmail.com>
2017-12-06 09:29:20 -0800

    Functionality added to change the status of burgers

--- public/assets/js/burger_script.js
+++ public/assets/js/burger_script.js
@@ -1,29 +1,44 @@
 
 $(function () {
 	$(".add-burger-form").on("submit",function (event) {
-	event.preventDefault();
+		event.preventDefault();
 
-    var newBurger = {
-      name: $("#txtArea").val().trim() 
-    };
+	    var newBurger = {
+	      name: $("#txtArea").val().trim() 
+	    };
 
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("Added a new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-	
-
-
-
-
+	    // Send the POST request.
+	    $.ajax("/api/burgers", {
+	      type: "POST",
+	      data: newBurger
+	    }).then(
+	      function() {
+	        console.log("Added a new burger");
+	        // Reload the page to get the updated list
+	        location.reload();
+	      }
+	    	);
 	});
+
+	$(".change-devoured").on("click",function (event) {
+	    
+	    var id=$(this).data("id");
+	    $.ajax("/api/burgers/"+id, {
+	      type: "PUT",
+	      data: id
+	    }).then(
+	      function() {
+	        console.log("Changed the satatus");
+
+	        // Reload the page to get the updated list
+	        location.reload();
+	      }
+	    	);
+	});
+
+
+
+
 
 
 
